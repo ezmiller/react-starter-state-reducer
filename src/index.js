a9aad46ece2f44bde9aff75bf1b68e6ecaf205e4
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-12-27 18:34:47 -0500

    Add skeleton css defaults

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./css/normalize.css";
+import "./css/skeleton.css";
+import "./css/index.css";
 import App from "./App";
 import stateReducer, { initialState } from "./state";
 import * as serviceWorker from "./serviceWorker";
